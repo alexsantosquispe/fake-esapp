cff35c79d6347b4d0fc25d13823a5fbe4bc45d18
Alex Santos <alexsantosquispe@gmail.com>
2025-10-29 18:51:59 -0400

    feat: cells with formtted values

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+export const TABLE_COLUMNS = [
+  'id',
+  'date',
+  'type',
+  'from',
+  'to',
+  'amount',
+  'status'
+];
+
+export const STATUS_COLORS = {
+  completed: 'bg-green-200',
+  pending: 'bg-yellow-200',
+  failed: 'bg-red-200'
+};
