76defaec9044c8e105418cac3c00c27387e1baba
Alex Santos <alexsantosquispe@gmail.com>
2025-10-31 11:12:15 -0400

    not found page

--- src/pages/NotFount.tsx
+++ src/pages/NotFount.tsx
@@ -1,5 +1,28 @@
+import { Button } from '../components/atoms/Button';
+import { useNavigate } from 'react-router';
+
 const NotFount = () => {
-  return <div>NotFount</div>;
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <div className="flex w-full flex-1 flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center gap-5">
+        <h1 className="text-3xl font-bold">Ooppss!</h1>
+        <p className="text-base font-medium">
+          Sorry we could not find the page you are looking for.
+        </p>
+      </div>
+      <Button
+        label="Back to home"
+        ariaLabel="Back to home"
+        onClick={goToHome}
+      />
+    </div>
+  );
 };
 
 export default NotFount;
